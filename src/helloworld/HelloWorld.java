579e40ea08b23b93cf9575e3b46d39b5b667bfce
bc591386 <bc591386@C42138.ens.uqam.ca>
2016-09-26 10:09:53 -0400

    Ajout de Bye World

--- src/helloworld/HelloWorld.java
+++ src/helloworld/HelloWorld.java
@@ -15,7 +15,8 @@ public class HelloWorld {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hello world!");    
+        System.out.println("Hello world!");   
+        System.out.println("Bye world!");  
     
     }
 }
